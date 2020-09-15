8887eadced25efd55308ec894aca9b5c3b205776
xnkevinnguyen <xuan-nam-kevin.nguyen@polymtl.ca>
2020-09-15 16:09:52 -0400

    Add appcenter

--- android/app/src/main/java/com/pixie/android/ui/draw/DrawActivity.kt
+++ android/app/src/main/java/com/pixie/android/ui/draw/DrawActivity.kt
@@ -1,12 +1,19 @@
 package com.pixie.android.ui.draw
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import com.pixie.android.R
 
 
 class DrawActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        AppCenter.start(
+            application, "47098cee-c78c-46cf-810f-bb72f246191f",
+            Analytics::class.java, Crashes::class.java
+        )
         super.onCreate(savedInstanceState)
 
         // Instantiates CanvasFragment and DrawToolsFragment
