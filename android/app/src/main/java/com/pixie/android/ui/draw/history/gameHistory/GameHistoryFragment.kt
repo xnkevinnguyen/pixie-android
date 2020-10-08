ea7e043f37ec76298bc2584807f191afbcbc930e
Sophie <sophie.dorval@polymtl.ca>
2020-10-08 13:14:09 -0400

    modified history fragment and added adpter

--- android/app/src/main/java/com/pixie/android/ui/draw/history/gameHistory/GameHistoryFragment.kt
+++ android/app/src/main/java/com/pixie/android/ui/draw/history/gameHistory/GameHistoryFragment.kt
@@ -3,8 +3,10 @@ package com.pixie.android.ui.draw.history.gameHistory
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.widget.ListView
 import androidx.fragment.app.DialogFragment
 import com.pixie.android.R
+import com.pixie.android.ui.draw.history.connectionHistory.ConnectionAdapter
 
 
 class GameHistoryFragment: DialogFragment() {
@@ -15,6 +17,11 @@ class GameHistoryFragment: DialogFragment() {
         val inflater = requireActivity().layoutInflater
         val layout: View = inflater.inflate(R.layout.game_history_fragment, null)
         builder.setContentView(layout)
+
+        val gameList = builder.findViewById<ListView>(R.id.history_game_list)
+        val gameAdapter = ConnectionAdapter(requireContext())
+        gameList.adapter = gameAdapter
+
         return builder
     }
 }
