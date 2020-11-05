49c11b0a92c13c8307fcd14652aa1ba8900b7f7e
xnkevinnguyen <xuan-nam-kevin.nguyen@polymtl.ca>
2020-11-04 21:30:29 -0500

    Fix crash because of undefined commands

--- android/app/src/main/java/com/pixie/android/data/draw/CanvasCommandHistoryRepository.kt
+++ android/app/src/main/java/com/pixie/android/data/draw/CanvasCommandHistoryRepository.kt
@@ -24,7 +24,6 @@ class CanvasCommandHistoryRepository {
 
         Log.d("CanvasCommandHistoryRepository","clear")
             drawCommandHistory.postValue(hashMapOf())
-            drawCommandHistory.notifyObserver()
 //        }
 
     }
