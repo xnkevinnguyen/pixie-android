69984bdd0a108a7de773a92b50e23d4c580cfc88
xnkevinnguyen <xuan-nam-kevin.nguyen@polymtl.ca>
2020-11-24 18:36:43 -0500

    Update unit tests

--- android/app/src/test/java/com/pixie/android/data/CanvasRepositoryTest.kt
+++ android/app/src/test/java/com/pixie/android/data/CanvasRepositoryTest.kt
@@ -66,7 +66,7 @@ class CanvasRepositoryTest {
     fun testAddManualDrawPoint_ShouldAddCommand(){
         //PREPARE
         val repository:CanvasRepository = getRepository()
-        val pointToAdd = ManualDrawingPoint(1000000.0,1f,1f,PathStatus.BEGIN)
+        val pointToAdd = ManualDrawingPoint(1000000.0,1.0,1f,1f,PathStatus.BEGIN)
         //PERFORM
 
         repository.addManualDrawPoint(pointToAdd)
@@ -81,8 +81,8 @@ class CanvasRepositoryTest {
     fun testAddManualDrawPoint_ShouldUpdateCommand(){
         //PREPARE
         val repository:CanvasRepository = getRepository()
-        val pointToAdd = ManualDrawingPoint(1000000.0,1f,1f,PathStatus.BEGIN)
-        val pointToAdd2 = ManualDrawingPoint(1000000.0,1f,1f,PathStatus.ONGOING)
+        val pointToAdd = ManualDrawingPoint(1000000.0,1.0,1f,1f,PathStatus.BEGIN)
+        val pointToAdd2 = ManualDrawingPoint(1000000.0,2.0,1f,1f,PathStatus.ONGOING)
         //PERFORM
 
         repository.addManualDrawPoint(pointToAdd)
