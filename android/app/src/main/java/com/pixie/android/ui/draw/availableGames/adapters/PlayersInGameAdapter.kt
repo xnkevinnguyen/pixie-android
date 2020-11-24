a54eadfb5359859f1c82330ad0008ec36927e931
Sophie <sophie.dorval@polymtl.ca>
2020-11-23 21:45:35 -0500

    modified leaderboard

--- android/app/src/main/java/com/pixie/android/ui/draw/availableGames/adapters/PlayersInGameAdapter.kt
+++ android/app/src/main/java/com/pixie/android/ui/draw/availableGames/adapters/PlayersInGameAdapter.kt
@@ -1,15 +1,23 @@
 package com.pixie.android.ui.draw.availableGames.adapters
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 import com.pixie.android.R
 import com.pixie.android.model.chat.ChannelParticipant
+import com.pixie.android.ui.draw.channelList.PlayersViewModel
+import com.pixie.android.utilities.Constants
+import com.pixie.android.utilities.InjectorUtils
 
 class PlayersInGameAdapter(context: Context) : BaseAdapter() {
 
@@ -17,6 +25,8 @@ class PlayersInGameAdapter(context: Context) : BaseAdapter() {
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     private var listOfPlayers = ArrayList<ChannelParticipant>()
+    val factory = InjectorUtils.providePlayersViewModelFactory()
+    val playersViewModel = ViewModelProvider(ViewModelStore(), factory).get(PlayersViewModel::class.java)
 
     fun add(channelParticipant: ChannelParticipant) {
         this.listOfPlayers.add(channelParticipant)
@@ -54,6 +64,16 @@ class PlayersInGameAdapter(context: Context) : BaseAdapter() {
 
         if(participant.isVirtual == false){
             removeVirtualElement.visibility = View.GONE
+        }
+
+        val ringElement = rowView.findViewById<ImageView>(R.id.avatar_ring)
+        if (playersViewModel.getFriendList().value?.contains(participant) == true) {
+            ringElement.backgroundTintList =
+                ColorStateList.valueOf(Color.parseColor(Constants.AVATAR_RING_COLOR_YELLOW))
+        } else {
+            ringElement.backgroundTintList =
+                ColorStateList.valueOf(Color.parseColor(Constants.AVATAR_RING_COLOR_SILVER))
+
         }
         return rowView
     }
